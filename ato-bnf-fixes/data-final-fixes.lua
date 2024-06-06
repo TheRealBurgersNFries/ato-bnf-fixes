249f2cb96fe0239c0b1af2d276e1fb8989323a0e
Shayne Hemminger <shaynehemminger@live.com>
2024-06-06 04:15:46 -0400

    Fixed SE-Space train capacities to double vanilla capacities Declared dependency for touched mods Hid SE-Space train Capacity Settings

--- ato-bnf-fixes/data-final-fixes.lua
+++ ato-bnf-fixes/data-final-fixes.lua
@@ -1,3 +1,4 @@
 
+
 data.raw["cargo-wagon"]["space-cargo-wagon"].inventory_size = 80
 data.raw["fluid-wagon"]["space-fluid-wagon"].capacity = 50000
---
+++ ato-bnf-fixes/settings-updates.lua
@@ -0,0 +1,2 @@
+data.raw["string-setting"]["space-fluid-wagon-capacity-setting"].hidden = true
+data.raw["string-setting"]["space-cargo-wagon-capacity-setting"].hidden = true
